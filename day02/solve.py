091412e7289f70f5988010b65ed2cb22f2fb17e2
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-12-02 11:37:00 -0500

    Solve day 2 part 2

--- day02/solve.py
+++ day02/solve.py
@@ -5,9 +5,7 @@ Advent of Code 2022 -- Day 2
 >>> part1(TEST_INPUT)
 15
 >>> part2(TEST_INPUT)
-Traceback (most recent call last):
-...
-NotImplementedError
+12
 """
 
 import re
@@ -22,19 +20,7 @@ C Z
 LINE_RE = re.compile(r'([ABC]) ([XYZ])')
 
 
-def part1(input):
-    scoring = {
-        ('A', 'X'): 1 + 3,
-        ('A', 'Y'): 2 + 6,
-        ('A', 'Z'): 3 + 0,
-        ('B', 'X'): 1 + 0,
-        ('B', 'Y'): 2 + 3,
-        ('B', 'Z'): 3 + 6,
-        ('C', 'X'): 1 + 6,
-        ('C', 'Y'): 2 + 0,
-        ('C', 'Z'): 3 + 3,
-    }
-
+def score(input, scoring):
     total = 0
     for line in input.splitlines():
         m = LINE_RE.fullmatch(line)
@@ -44,8 +30,32 @@ def part1(input):
     return total
 
 
+def part1(input):
+    return score(input, {
+        ('A', 'X'): 1 + 3,
+        ('A', 'Y'): 2 + 6,
+        ('A', 'Z'): 3 + 0,
+        ('B', 'X'): 1 + 0,
+        ('B', 'Y'): 2 + 3,
+        ('B', 'Z'): 3 + 6,
+        ('C', 'X'): 1 + 6,
+        ('C', 'Y'): 2 + 0,
+        ('C', 'Z'): 3 + 3,
+    })
+
+
 def part2(input):
-    raise NotImplementedError
+    return score(input, {
+        ('A', 'X'): 3 + 0,
+        ('A', 'Y'): 1 + 3,
+        ('A', 'Z'): 2 + 6,
+        ('B', 'X'): 1 + 0,
+        ('B', 'Y'): 2 + 3,
+        ('B', 'Z'): 3 + 6,
+        ('C', 'X'): 2 + 0,
+        ('C', 'Y'): 3 + 3,
+        ('C', 'Z'): 1 + 6,
+    })
 
 
 def main(args):
