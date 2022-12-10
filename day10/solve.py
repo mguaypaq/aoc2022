f36adad920a13ad4118e47c569ad09e626adc9de
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-12-10 11:28:30 -0500

    Solve day 10 part 1

---
+++ day10/solve.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python3
+"""
+Advent of Code 2022 -- Day 10
+
+>>> part1(TEST_INPUT)
+13140
+>>> part2(TEST_INPUT)
+Traceback (most recent call last):
+...
+NotImplementedError
+>>> list(execute(TEST_INPUT))[19::40]
+[21, 19, 18, 21, 16, 18]
+"""
+
+import re
+import sys
+
+addx_re = re.compile(r'addx (0|-?[1-9]\d*)')
+
+
+def execute(input):
+    x = 1
+    for line in input.splitlines():
+        if line == 'noop':
+            yield x
+        elif m := addx_re.fullmatch(line):
+            yield x
+            yield x
+            x += int(m[1])
+        else:
+            raise ValueError(f'invalid instruction {line!r}')
+
+
+def part1(input):
+    return sum(
+        cycle * x
+        for cycle, x in enumerate(execute(input), start=1)
+        if cycle % 40 == 20
+    )
+
+
+def part2(input):
+    raise NotImplementedError
+
+
+def main(args):
+    input = open('input.txt').read()
+    if args == ['1']:
+        print(part1(input), file=open('output1.txt', 'w'))
+    elif args == ['2']:
+        print(part2(input), file=open('output2.txt', 'w'))
+    else:
+        raise ValueError(f'invalid arguments: {args!r}')
+
+
+TEST_INPUT = """\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
