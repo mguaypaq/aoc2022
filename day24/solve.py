845865547df9546fa075c2305f8298535b1e3e53
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-12-24 11:22:28 -0500

    Solve day 24 part 2

--- day24/solve.py
+++ day24/solve.py
@@ -5,9 +5,7 @@ Advent of Code 2022 -- Day 24
 >>> part1(TEST_INPUT)
 18
 >>> part2(TEST_INPUT)
-Traceback (most recent call last):
-...
-NotImplementedError
+54
 """
 
 import re
@@ -26,25 +24,22 @@ cliff_re = re.compile(r'#+(?P<hole>\.)#+')
 valley_re = re.compile(r'#(?P<hole>[>v<^.]+)#')
 
 
-def part1(input):
+def parse(input):
     lines = input.splitlines()
     if len(lines) < 3:
         raise ValueError('not enough lines')
     if any(len(line) != len(lines[0]) for line in lines):
         raise ValueError('uneven lines')
 
-    height = len(lines) - 2
-    width = len(lines[0]) - 2
-
     top, *mid, bot = lines
     m = cliff_re.fullmatch(top)
     if m is None:
         raise ValueError('invalid top line')
-    start = (m.start('hole')-1, -1)
+    start = (m.start('hole')-1, 0)
     m = cliff_re.fullmatch(bot)
     if m is None:
         raise ValueError('invalid bottom line')
-    goal = (m.start('hole')-1, height-1)
+    goal = (m.start('hole')-1, len(mid)-1)
 
     board = []
     for line in mid:
@@ -53,16 +48,21 @@ def part1(input):
             raise ValueError('invalid middle line')
         board.append(m['hole'])
 
-    time = 0
+    return start, goal, board
+
+
+def walk(time, start, goal, board):
+    height = len(board)
+    width = len(board[0])
     current = set()
     while goal not in current:
         time += 1
-        current.add(start)
         current = {
             (x+dx, y+dy)
             for x, y in current
             for dx, dy in [(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
         }
+        current.add(start)
         current = {
             (x, y)
             for x, y in current
@@ -76,8 +76,16 @@ def part1(input):
     return time + 1
 
 
+def part1(input):
+    start, goal, board = parse(input)
+    return walk(0, start, goal, board)
+
+
 def part2(input):
-    raise NotImplementedError
+    start, goal, board = parse(input)
+    time = walk(0, start, goal, board)
+    time = walk(time, goal, start, board)
+    return walk(time, start, goal, board)
 
 
 def main(args):
