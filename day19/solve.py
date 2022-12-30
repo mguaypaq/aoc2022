d2f0634d7a76a70ee51de70f4de858d9a85d574b
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-12-30 01:09:39 -0500

    Solve day 19 part 2
    This one was hard!

--- day19/solve.py
+++ day19/solve.py
@@ -2,15 +2,20 @@
 """
 Advent of Code 2022 -- Day 19
 
-# too slow:
-# >>> part1(TEST_INPUT)
-# 33
->>> part2(TEST_INPUT)
-Traceback (most recent call last):
-...
-NotImplementedError
+>>> bp1, bp2 = map(Blueprint.parse, TEST_INPUT.splitlines())
+>>> bp1.max_geodes(24)
+9
+>>> bp2.max_geodes(24)
+12
+>>> bp1.max_geodes(32)
+56
+>>> bp2.max_geodes(32)
+62
 """
 
+from heapq import heappush, heappop, heapify
+from itertools import combinations
+from math import prod
 import re
 import sys
 from typing import NamedTuple
@@ -20,14 +25,12 @@ TEST_INPUT = (
     "Each ore robot costs 4 ore. "
     "Each clay robot costs 2 ore. "
     "Each obsidian robot costs 3 ore and 14 clay. "
-    "Each geode robot costs 2 ore and 7 obsidian."
-    "\n"
+    "Each geode robot costs 2 ore and 7 obsidian.\n"
     "Blueprint 2: "
     "Each ore robot costs 2 ore. "
     "Each clay robot costs 3 ore. "
     "Each obsidian robot costs 3 ore and 8 clay. "
-    "Each geode robot costs 3 ore and 12 obsidian."
-    "\n"
+    "Each geode robot costs 3 ore and 12 obsidian.\n"
 )
 
 line_re = re.compile(
@@ -38,93 +41,235 @@ line_re = re.compile(
     r'Each geode robot costs ([1-9]\d*) ore and ([1-9]\d*) obsidian.'
 )
 
-infty = float('inf')
-
 
 class Blueprint(NamedTuple):
     ident: int
-    ore_ore: int
-    clay_ore: int
-    obs_ore: int
-    obs_clay: int
-    geode_ore: int
-    geode_obs: int
+    rock_rock: int
+    clay_rock: int
+    obsi_rock: int
+    obsi_clay: int
+    geod_rock: int
+    geod_obsi: int
+
+    @classmethod
+    def parse(cls, line):
+        m = line_re.fullmatch(line)
+        if m is None:
+            raise ValueError(f'bad line: {line!r}')
+        return cls(*map(int, m.groups()))
+
+    def max_geodes(self, time):
+        states = []
+        initial_supplies = Supplies()
+        for rock_times in self.valid_rock_times(time):
+            rock_schedule = self.rock_schedule(time, rock_times)
+            states.append((
+                -initial_supplies.bound(self, rock_times, rock_schedule),
+                initial_supplies,
+                rock_times,
+                rock_schedule,
+            ))
+        heapify(states)
+
+        while True:
+            state = heappop(states)
+            bound = -state[0]
+            supplies, rock_times, rock_schedule = state[1:]
+            if supplies.feasible(time) == bound:
+                return bound
+
+            if supplies.clay_bots < self.obsi_clay:
+                current = supplies
+                while current.time < time:
+                    if current.time in rock_times:
+                        current = current.wait(rock_schedule)
+                    elif current.can_build_clay(self):
+                        current = current.wait(rock_schedule)
+                        current = current.build_clay(self)
+                        heappush(states, (
+                            -current.bound(self, rock_times, rock_schedule),
+                            current,
+                            rock_times,
+                            rock_schedule,
+                        ))
+                        break
+                    else:
+                        current = current.wait(rock_schedule)
+
+            if supplies.clay_bots > 0 and supplies.obsi_bots < self.geod_obsi:
+                current = supplies
+                while current.time < time:
+                    if current.time in rock_times:
+                        current = current.wait(rock_schedule)
+                    elif current.can_build_obsi(self):
+                        current = current.wait(rock_schedule)
+                        current = current.build_obsi(self)
+                        heappush(states, (
+                            -current.bound(self, rock_times, rock_schedule),
+                            current,
+                            rock_times,
+                            rock_schedule,
+                        ))
+                        break
+                    else:
+                        current = current.wait(rock_schedule)
+
+            if supplies.obsi_bots > 0:
+                current = supplies
+                while current.time < time:
+                    if current.time in rock_times:
+                        current = current.wait(rock_schedule)
+                    elif current.can_build_geod(self):
+                        current = current.wait(rock_schedule)
+                        current = current.build_geod(self)
+                        heappush(states, (
+                            -current.bound(self, rock_times, rock_schedule),
+                            current,
+                            rock_times,
+                            rock_schedule,
+                        ))
+                        break
+                    else:
+                        current = current.wait(rock_schedule)
+
+    def valid_rock_times(self, time):
+        max_rock = max(self.clay_rock, self.obsi_rock, self.geod_rock)
+        for n in range(max_rock):
+            for rock_times in combinations(range(time), n):
+                if all(
+                    sum(rock_times[:i]) + i <= (t - self.rock_rock) * (i+1)
+                    for i, t in enumerate(rock_times)
+                ):
+                    yield rock_times
+
+    def rock_schedule(self, time, rock_times):
+        rs = [1] * time
+        for t in rock_times:
+            for i in range(t+1, time):
+                rs[i] += 1
+            t -= 1
+            for _ in range(self.rock_rock):
+                while rs[t] == 0:
+                    t -= 1
+                    assert t >= 0
+                rs[t] -= 1
+        return rs
 
 
-class State(NamedTuple):
-    ore_robot: int = 1
-    clay_robot: int = 0
-    obs_robot: int = 0
-    geode_robot: int = 0
-    ore: int | float = 0
-    clay: int | float = 0
-    obs: int | float = 0
-    geode: int = 0
+class Supplies(NamedTuple):
+    time: int = 0
+    clay_bots: int = 0
+    obsi_bots: int = 0
+    geod_bots: int = 0
+    rock: int = 0
+    clay: int = 0
+    obsi: int = 0
+    geod: int = 0
 
-    def __le__(self, other):
-        return all(s <= o for s, o in zip(self, other))
-
-    def __lt__(self, other):
-        return self != other and self <= other
-
-    def successors(self, bp):
-        (ore_robot, clay_robot, obs_robot, geode_robot,
-            old_ore, old_clay, old_obs, geode) = self
-
-        max_ore = max(bp.ore_ore, bp.clay_ore, bp.obs_ore, bp.geode_ore)
-        ore = old_ore + ore_robot if ore_robot < max_ore else infty
-        clay = old_clay + clay_robot if clay_robot < bp.obs_clay else infty
-        obs = old_obs + obs_robot if obs_robot < bp.geode_obs else infty
-        geode += geode_robot
-
-        yield State(
-            ore_robot, clay_robot, obs_robot, geode_robot,
-            ore, clay, obs, geode,
+    def wait(self, rock_schedule):
+        return self._replace(
+            time=self.time + 1,
+            rock=self.rock + rock_schedule[self.time],
+            clay=self.clay + self.clay_bots,
+            obsi=self.obsi + self.obsi_bots,
+            geod=self.geod + self.geod_bots,
         )
-        if ore < infty and bp.ore_ore <= old_ore:
-            yield State(
-                ore_robot + 1, clay_robot, obs_robot, geode_robot,
-                ore - bp.ore_ore, clay, obs, geode,
-            )
-        if clay < infty and bp.clay_ore <= old_ore:
-            yield State(
-                ore_robot, clay_robot + 1, obs_robot, geode_robot,
-                ore - bp.clay_ore, clay, obs, geode,
-            )
-        if obs < infty and bp.obs_ore <= old_ore and bp.obs_clay <= old_clay:
-            yield State(
-                ore_robot, clay_robot, obs_robot + 1, geode_robot,
-                ore - bp.obs_ore, clay - bp.obs_clay, obs, geode,
-            )
-        if bp.geode_ore <= old_ore and bp.geode_obs <= old_obs:
-            yield State(
-                ore_robot, clay_robot, obs_robot, geode_robot + 1,
-                ore - bp.geode_ore, clay, obs - bp.geode_obs, geode,
-            )
+
+    def can_build_clay(self, bp):
+        return self.rock >= bp.clay_rock
+
+    def build_clay(self, bp):
+        return self._replace(
+            clay_bots=self.clay_bots + 1,
+            rock=self.rock - bp.clay_rock,
+        )
+
+    def can_build_obsi(self, bp):
+        return self.rock >= bp.obsi_rock and self.clay >= bp.obsi_clay
+
+    def build_obsi(self, bp):
+        return self._replace(
+            obsi_bots=self.obsi_bots + 1,
+            rock=self.rock - bp.obsi_rock,
+            clay=self.clay - bp.obsi_clay,
+        )
+
+    def can_build_geod(self, bp):
+        return self.rock >= bp.geod_rock and self.obsi >= bp.geod_obsi
+
+    def build_geod(self, bp):
+        return self._replace(
+            geod_bots=self.geod_bots + 1,
+            rock=self.rock - bp.geod_rock,
+            obsi=self.obsi - bp.geod_obsi,
+        )
+
+    def feasible(self, time):
+        return self.geod + self.geod_bots * (time - self.time)
+
+    def bound(self, bp, rock_times, rock_schedule):
+        times = range(self.time, len(rock_schedule))
+        rock_schedule = rock_schedule[self.time:]
+
+        clay_bots = self.clay_bots
+        rock = self.rock
+        clay_schedule = []
+        for t, new_rock in zip(times, rock_schedule):
+            clay_schedule.append(clay_bots)
+            if rock >= bp.clay_rock and t not in rock_times:
+                clay_bots += 1
+                rock -= bp.clay_rock
+            rock += new_rock
+
+        obsi_bots = self.obsi_bots
+        rock = self.rock
+        clay = self.clay
+        obsi_schedule = []
+        for t, new_rock, new_clay in zip(times, rock_schedule, clay_schedule):
+            obsi_schedule.append(obsi_bots)
+            if (
+                rock >= bp.obsi_rock
+                and clay >= bp.obsi_clay
+                and t not in rock_times
+            ):
+                obsi_bots += 1
+                rock -= bp.obsi_rock
+                clay -= bp.obsi_clay
+            rock += new_rock
+            clay += new_clay
+
+        geod_bots = self.geod_bots
+        rock = self.rock
+        obsi = self.obsi
+        geod = self.geod
+        for t, new_rock, new_obsi in zip(times, rock_schedule, obsi_schedule):
+            geod += geod_bots
+            if (
+                rock >= bp.geod_rock
+                and obsi >= bp.geod_obsi
+                and t not in rock_times
+            ):
+                geod_bots += 1
+                rock -= bp.geod_rock
+                obsi -= bp.geod_obsi
+            rock += new_rock
+            obsi += new_obsi
+        return geod
 
 
 def part1(input):
     quality = 0
     for line in input.splitlines():
-        m = line_re.fullmatch(line)
-        if m is None:
-            raise ValueError(f'bad line: {line!r}')
-        bp = Blueprint(*map(int, m.groups()))
-        states = {State()}
-        for _ in range(24):
-            new_states = set()
-            for s in states:
-                new_states.update(s.successors(bp))
-            states = {
-                s for s in new_states
-                if not any(s < t for t in new_states)
-            }
-        quality += bp.ident * max(s.geode for s in states)
+        bp = Blueprint.parse(line)
+        quality += bp.ident * bp.max_geodes(24)
     return quality
 
 
 def part2(input):
-    raise NotImplementedError
+    return prod(
+        Blueprint.parse(line).max_geodes(32)
+        for line in input.splitlines()[:3]
+    )
 
 
 def main(args):
